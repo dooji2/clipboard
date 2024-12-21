bbcd76539303b0ee8131effca482855ec90653c5
dooji2 <cmir79411@gmail.com>
2024-12-21 21:32:21 +0200

    Render background

--- src/main/java/com/dooji/clipboard/ui/ClipboardScreen.java
+++ src/main/java/com/dooji/clipboard/ui/ClipboardScreen.java
@@ -164,6 +164,7 @@ public class ClipboardScreen extends Screen {
 
     @Override
     public void render(DrawContext context, int mouseX, int mouseY, float delta) {
+        super.renderBackground(context);
         super.render(context, mouseX, mouseY, delta);
 
         int titleHeight = this.textRenderer.fontHeight;
